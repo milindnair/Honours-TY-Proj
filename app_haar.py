0c31162fc3237123c8e731ba142d9560f108436e
Aditya Parikh <asparikh03@gmail.com>
2025-03-24 01:18:18 +0530

    added haar cascading for face detection

--- app_haar.py
+++ app_haar.py
@@ -29,13 +29,6 @@ def extract_frames(video_path):
     cap.release()
     return frames
 
-# def detect_with_haar(frame):
-#     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     detections = haar_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-#     for (x, y, w, h) in detections:
-#         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Blue rectangle for detected objects
-#     return frame, len(detections) > 0
-
 
 def detect_with_haar(frame):
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
